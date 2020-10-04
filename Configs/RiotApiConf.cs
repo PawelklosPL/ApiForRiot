7db588db3f503b8128fb1eb85c003ba36db27fda
lecytynaSojowa <50423642+lecytynaSojowa@users.noreply.github.com>
2020-10-04 19:54:59 +0200

    stats and items
    summoner stats and items now display properly

--- Configs/RiotApiConf.cs
+++ Configs/RiotApiConf.cs
@@ -2,6 +2,6 @@ namespace riotApi.Configs
 {
     public class RiotApiConfiguration
     {
-        public static string RiotApiKey = "RGAPI-b1e99a5c-29b9-4922-94d8-dc6665306470";
+        public static string RiotApiKey = "RGAPI-16330fe6-f617-4085-98a7-e19db8da7ca9";
     }
 }
